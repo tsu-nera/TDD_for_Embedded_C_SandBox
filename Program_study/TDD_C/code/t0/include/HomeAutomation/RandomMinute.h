116e29ca16f6ec8e2cc8b1e278f5e28a82a5ff76
TSUNEMICHI <TSUNEMICHI@TSUNEMICHI-PC>
2011-10-25 23:53:49 +0900

    t0初回登録

---
+++ Program_study/TDD_C/code/t0/include/HomeAutomation/RandomMinute.h
@@ -0,0 +1,34 @@
+/*- ------------------------------------------------------------------ -*/
+/*-    Copyright (c) James W. Grenning -- All Rights Reserved          -*/
+/*-    For use by owners of Test-Driven Development for Embedded C,    -*/
+/*-    and attendees of Renaissance Software Consulting, Co. training  -*/
+/*-    classes.                                                        -*/
+/*-                                                                    -*/
+/*-    Available at http://pragprog.com/titles/jgade/                  -*/
+/*-        ISBN 1-934356-62-X, ISBN13 978-1-934356-62-3                -*/
+/*-                                                                    -*/
+/*-    Authorized users may use this source code in your own           -*/
+/*-    projects, however the source code may not be used to            -*/
+/*-    create training material, courses, books, articles, and         -*/
+/*-    the like. We make no guarantees that this source code is        -*/
+/*-    fit for any purpose.                                            -*/
+/*-                                                                    -*/
+/*-    www.renaissancesoftware.net james@renaissancesoftware.net       -*/
+/*- ------------------------------------------------------------------ -*/
+
+
+#ifndef D_RandomMinute_H
+#define D_RandomMinute_H
+
+
+/* START: Interface2 */
+void RandomMinute_Create(int bound);
+extern int (*RandomMinute_Get)(void);
+/* END: Interface2 */
+
+#if 0 /* START: Interface1 */
+void RandomMinute_Create(int bound);
+int RandomMinute_Get(void);
+#endif /* END: Interface1 */
+
+#endif  /* D_RandomMinute_H */
